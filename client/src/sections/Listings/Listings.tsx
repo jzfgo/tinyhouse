ce5b1fbd6993aa97d9fedb63868db67399988e1a
Javier Zapata <javierzapata82@gmail.com>
2021-01-23 21:05:08 +0100

    Module 6

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,57 @@
+import { server } from '../../lib/api';
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from './types';
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data.listings);
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({ query: DELETE_LISTING, variables: { id: '60047585f0d7e86923c85355' } });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
+    </div>
+  );
+};
